27e8fcf363266a952ad7266f72270940c6d81a3b
Aissa54 <95747202+Aissa54@users.noreply.github.com>
2022-03-16 16:37:18 +0100

    api

---
+++ API/api.py
@@ -0,0 +1,41 @@
+from traceback import print_exc
+from flask import Flask
+from flask.views import MethodView
+import marshmallow as ma
+from flask_rest_api import Api, Blueprint, abort
+from pip import main
+
+
+app = Flask('My API')
+app.config['OPENAPI_VERSION'] = '3.0.2'
+api = Api(app)
+
+
+class VoitureSchema(ma.Schema):
+    marque = ma.fields.String()
+    modele = ma.fields.String()
+    prix = ma.fields.String()
+
+
+class PredictionPrixSchema(ma.Schema):
+    prix = ma.fields.Int()
+
+
+blp = Blueprint(
+    'voiture', 'prediction', url_prefix='/carprediction',
+    description="prediction vente voiture d'ocasion"
+)
+
+
+@blp.route('/')
+class Prediction(MethodView):
+    @blp.response(PredictionPrixSchema(many=True))
+    def get(self):
+        return "bonjour"
+
+
+api.register_blueprint(blp)
+
+if __name__== "__main__":
+    app.run()
+    
