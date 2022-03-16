3b8a122a08867e0ef2475242bd0b67f321ad4dfe
yannis <yannis54400@gmail.com>
2022-03-16 11:51:15 +0100

    list urls.txt

--- scraping/scraping/spiders/catalogue.py
+++ scraping/scraping/spiders/catalogue.py
@@ -1,29 +1,33 @@
 import requests
 from requests import get
 from bs4 import BeautifulSoup
-import pandas as pd
-import numpy as np
-
-from time import sleep
-from random import randint
-
+import json 
+import unidecode
+f=open("../../marque_model.json")
+marques_modeles=json.load(f)
+print(marques_modeles)
 listes_annonces = []
-url="https://www.paruvendu.fr/a/voiture-occasion/audi/?p="
+for marque,modele in marques_modeles.items():
 
-for i in range(1,501):
+    url=f"https://www.paruvendu.fr/a/voiture-occasion/{marque}/"
+    for each in modele:
+        new_url=url+unidecode.unidecode(each)
 
-
-    page = requests.get(url+str(i))
-    if page.ok:
-
-        soup = BeautifulSoup(page.text, 'html.parser')
-
-        divs= soup.find_all('div', class_='lazyload_bloc ergov3-annonce ergov3-annonceauto')
-        for div in divs:
-            listes_annonces.append(div.find('a').get("href"))
-
-print(len(listes_annonces))
+        for i in range(1,2):
 
 
+            page = requests.get(url+"/?p="+str(i))
+            if page.ok:
+
+                soup = BeautifulSoup(page.text, 'html.parser')
+
+                divs= soup.find_all('div', class_='lazyload_bloc ergov3-annonce ergov3-annonceauto')
+                for div in divs:
+                    listes_annonces.append(div.find('a').get("href"))
+
+
+
+with open("urls.txt", "w") as opfile:
+    opfile.write("\n".join(listes_annonces))
 
 
