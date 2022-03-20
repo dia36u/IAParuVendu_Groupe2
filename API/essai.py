f29c351a5c0bf7307ea16034e7fb79800e3913e8
yannismxflux <47094289+yannismxflux@users.noreply.github.com>
2022-03-20 17:23:14 +0100

    api finish

--- API/essai.py
+++ API/essai.py
@@ -1,44 +1,63 @@
+from pydantic import BaseModel
+import pandas as pd
 from fastapi import FastAPI
-from matplotlib.pyplot import get
-
+from joblib import load
 app = FastAPI()
 
+with open("tree.joblib", "rb") as f:
+    model = load(f)
+    f.close()
 
-@app.get("/")
-async def get_Home():
-    return {'message': "bienvenu sur la page de prediction"}, 200
+
+class Car(BaseModel):
+    marque: str
+    modele: str
+    date_fabrication: int
+    kilometrage: int
+    carrosserie: str
+    code_postal: str
+    energie: str
+    emission: int
+    consommation: float
+    transmission: str
+    portes: int
+    sieges: int
+    cv_fiscaux: int
+    cv_reels: int
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "marque": "fiat",
+                "modele": "500",
+                "date_fabrication": 2013,
+                "kilometrage": 82100,
+                "carrosserie": "berline",
+                "code_postal": 87000,
+                "energie": "essence",
+                "emission": 92,
+                "consommation": 4.0,
+                "transmission": "manuelle",
+                "portes": 2,
+                "sieges": 4,
+                "cv_fiscaux": 4,
+                "cv_reels": 85
+            }
+        }
 
 
-@app.post("/prediction")
-async def post_prediction(version: str,
+@app.get('/')
+def welcome():
+    """This api was made for scholar training and provide a prediction about second hand car price in France
+    """
 
-                          marque: str,
 
-                          modele: str,
-
-                          date_fabrication: str,
-
-                          kilometrage: str,
-
-                          couleur: str,
-
-                          carrosserie: str,
-
-                          code_postal: str,
-
-                          energie: str,
-
-                          emission: str,
-
-                          consommation: str,
-
-                          transmission: str,
-
-                          portes: str,
-
-                          cv_fiscaux: str,
-
-                          cv_reels: str,
-
-                          prix_vente: str):
-    return {"prix": 2300}
+@app.post('/prediction/')
+async def pred(data: Car):
+    """
+    Route permettant de faire la prediction du prix d'une voiture grace a ses caracteristique
+    """
+    car = pd.DataFrame(data.dict(), index=[0])
+    price = model.predict(car)
+    print(price)
+    return {'price': price[0]}
